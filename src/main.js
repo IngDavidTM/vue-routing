74d047717616a0dc4ab87c82f3312c43ea2efb23
IngDavidTM <david-1011@hotmail.com>
2024-08-28 16:11:56 -0500

    Render multiple routes

--- src/main.js
+++ src/main.js
@@ -6,21 +6,33 @@ import TeamsList from './components/teams/TeamsList.vue';
 import UsersList from './components/users/UsersList.vue';
 import TeamMembers from './components/teams/TeamMembers.vue';
 import NotFound from './components/nav/NotFound.vue';
+import TeamsFooter from './components/teams/TeamsFooter.vue';
+import UsersFooter from './components/users/UsersFooter.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', redirect: '/teams' },
-    { name: 'teams', path: '/teams', component: TeamsList, children: [
-      { name: 'team-members', path: ':teamId', component: TeamMembers, props: true },
-    ] },
-    { path: '/users', component: UsersList },
-    { path: '/:notFound(.*)', component: NotFound}
+    {
+      name: 'teams',
+      path: '/teams',
+      components: { default: TeamsList, footer: TeamsFooter },
+      children: [
+        {
+          name: 'team-members',
+          path: ':teamId',
+          component: TeamMembers,
+          props: true,
+        },
+      ],
+    },
+    { path: '/users', components: { default: UsersList, footer: UsersFooter } },
+    { path: '/:notFound(.*)', component: NotFound },
   ],
   linkActiveClass: 'active',
 });
 
-const app = createApp(App)
+const app = createApp(App);
 
 app.use(router);
 
